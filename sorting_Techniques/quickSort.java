541181de070402b75d82197fd0eb4c911b04c62b
saikrishna899 <211310563@gvpce.ac.in>
2025-01-03 22:55:09 +0530

    added quicsort code

---
+++ sorting_Techniques/quickSort.java
@@ -0,0 +1,41 @@
+import java.util.Arrays;
+
+public class quickSort {
+    public static void main(String[] args) {
+        int[] arr = {1, 5, 3, 0 ,9 ,2, 3};
+        sort(arr, 0, arr.length-1);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    static void sort(int[] nums, int low, int high){
+        if(low >= high){
+            return;
+        }
+        int pivotIndex = partition(nums, low, high);
+        sort(nums, low, pivotIndex-1);
+        sort(nums, pivotIndex+1, high);
+    }
+
+    static int partition(int[] nums, int low, int high){
+        int pivot = nums[low];
+        int i = low;
+        int j = high;
+        while(i < j){
+            while(nums[i] <= pivot && i < high){
+                i++;
+            }
+            while(nums[j] > pivot && j > low){
+                j--;
+            }
+            if(i < j){
+                int temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
+            }
+        }
+        int temp = nums[j];
+        nums[j] = nums[low];
+        nums[low] = temp;
+        return j;
+    }
+}
